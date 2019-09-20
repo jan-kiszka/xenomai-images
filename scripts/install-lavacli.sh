dcc7e24158ef4bbf090ae20514796ac4e226e28a
Quirin Gylstorff <quirin.gylstorff@siemens.com>
2019-09-20 10:46:29 +0200

    ci: use script to install lavacli.
    lavacli is not part of the used image and need to be installed.
    Due to changes in the lavacli dependencies it is not possible
    to connect to the master installed via apt, therefore use
    the pip3 version in case of Debian stretch. If Image release
    is Debian 10 or newer use apt else use pip3.
    
    This patch fixes a regression after the kasproject
    downgraded the docker base image from Debian 10 to Debian 9.
    
    Signed-off-by: Quirin Gylstorff <quirin.gylstorff@siemens.com>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

---
+++ scripts/install-lavacli.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Xenomai Real-Time System
+#
+# Copyright (c) Siemens AG, 2019
+#
+# Authors:
+#  Quirin Gylstorff <quirin.gylstorff@siemens.com>
+#
+# SPDX-License-Identifier: MIT
+#
+# check if Debian 10 or later. If not use the pip version
+# of lavacli. The apt version from stretch-backports cannot communicate
+# with the lava master on Debian 10.
+os_version_id=$(sed -nr 's/VERSION_ID="([0-9]+)"/\1/p' /etc/os-release)
+if [ "${os_version_id}" -ge "10" ]; then
+    sudo apt update
+    sudo apt install -y lavacli
+else
+    sudo pip3 install lavacli
+fi
